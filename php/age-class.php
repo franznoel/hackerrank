ce0352ed5e947a31797e66027c5404f4bf2f0bd2
Franz Noel Tanglao <franz@asteriainteractive.com>
2016-08-29 23:00:12 -0700

    php:fix age class.

--- php/age-class.php
+++ php/age-class.php
@@ -1,25 +1,23 @@
 <?php
 class Person{
-    public $age;
+   public $age;
    public function __construct($initialAge){
           // Add some more code to run some checks on initialAge
        if($initialAge<0) {
            $this->age = 0;
-           print("Age is not valid, setting age to 0.". $this->age ."\n");
+           print("Age is not valid, setting age to 0.\n");
+       } else {
+           $this->age = $initialAge;
        }
-
-       // global $T;
-       // print($T);
-
     }
-   public  function amIOld() {
+   public  function amIOld(){
             // Do some computations in here and print out the correct statement to the console 
        if($this->age < 13) {
-           print("You are young.". $this->age ."\n");
-       } else if($this->age >= 13 && $this->age <= 18) {
-           print("You are a teenager.". $this->age ."\n");
+           print("You are young.\n");
+       } else if($this->age >= 13 && $this->age < 18) {
+           print("You are a teenager.\n");
        } else {
-           print ("You are old.". $this->age ."\n");
+           print ("You are old.\n");
        }
     }
    public  function yearPasses(){
